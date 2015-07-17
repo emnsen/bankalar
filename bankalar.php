2138609314d0dcb059b662ac3fb9d3a9f3b17cfa
Emin Şen <emnsen@gmail.com>
2015-07-17 10:49:08 +0300

    bankalar

---
+++ bankalar.php
@@ -0,0 +1,473 @@
+<?php
+$banks = [
+    [
+        "name" => "Adabank A.Ş.",
+        "address" => "Büyükdere Cad. Rumelihan No 40 Kat 2 Mecidiyeköy İstanbul",
+        "telephone" => "212-2726420",
+        "fax" => "212-2726446",
+        "web" => "http://www.adabank.com.tr",
+        "telex" => "27282 adb TR",
+        "eft" => "0100",
+        "swift" => "ADABTRIS"
+    ],
+    [
+        "name" => "Akbank T.A.Ş.",
+        "address" => "Sabancı Center 4. Levent İstanbul",
+        "telephone" => "212-3855555",
+        "fax" => "212-2697383",
+        "web" => "http://www.akbank.com",
+        "telex" => "26449 aktc TR",
+        "eft" => "0046",
+        "swift" => "AKBKTRIS"
+  ],
+  [
+      "name" => "Aktif Yatırım Bankası A.Ş.",
+        "address" => "Büyükdere Cad. No 163 Zincirlikuyu Şişli İstanbul",
+        "telephone" => "212-3408000",
+        "fax" => "212-3408987",
+        "web" => "http://www.aktifbank.com.tr",
+        "telex" => null,
+        "eft" => "0143",
+        "swift" => "CAYTTRIS"
+  ],
+  [
+      "name" => "Alternatifbank A.Ş.",
+      "address" => "Cumhuriyet Caddesi No. 46 Elmadağ Şişli İstanbul",
+      "telephone" => "212-3156500",
+      "fax" => "212-2331500",
+      "web" => "http://www.abank.com.tr",
+      "telex" => "26191 ALFBTRIS",
+      "eft" => "0124",
+      "swift" => "ALFBTRIS"
+  ],
+  [
+      "name" => "Anadolubank A.Ş.",
+      "address" => "Cumhuriyet Mah. Silahşör Cad. No 69 Bomonti Şişli İstanbul",
+      "telephone" => "212-3687000",
+      "fax" => "212-2965715",
+      "web" => "http://www.anadolubank.com.tr",
+      "telex" => null,
+      "eft" => "0135",
+      "swift" => "ANDLTRIS"
+  ],
+  [
+      "name" => "Arap Türk Bankası A.Ş.",
+      "address" => "Valikonağı Cad. No 10 Nişantaşı İstanbul",
+      "telephone" => "212-225050020",
+      "fax" => "212-22505262249992",
+      "web" => "http://www.atbank.com.tr",
+      "telex" => "26830 atbk TR",
+      "eft" => "0091",
+      "swift" => "ATUBTRIS"
+  ],
+  [
+      "name" => "Bank Mellat",
+      "address" => "Büyükdere Cad. Binbirçiçek Sok. No 1 1.Levent İstanbul",
+      "telephone" => "212-2798015",
+      "fax" => "212-28462142846657",
+      "web" => "http://www.mellatbank.com",
+      "telex" => "26502 melt TR",
+      "eft" => "0094",
+      "swift" => "BKMTTRIS"
+  ],
+  [
+      "name" => "Bank of Tokyo-Mitsubishi UFJ Turkey A.Ş.",
+      "address" => "Fatih Sultan Mehmet Mahallesi, Poligon Caddesi Buyaka 2 Sitesi No: 8B C-Blok, Kat: 20-21 Tepeüstü Ümraniye İstanbul",
+      "telephone" => "216-6003000",
+      "fax" => "216-2906473",
+      "web" => "http://www.tu.bk.mufg.jp",
+      "telex" => null,
+      "eft" => "0147",
+      "swift" => "BOTKTRIS"
+  ],
+  [
+      "name" => "BankPozitif Kredi ve Kalkınma Bankası A.Ş.",
+      "address" => "Rüzgarlıbahçe Mah. Kayın Sok. No.3 Kavacık Beykoz İstanbul",
+      "telephone" => "216-5382525",
+      "fax" => "216-5384258",
+      "web" => "http://www.bankpozitif.com.tr",
+      "telex" => null,
+      "eft" => "0142",
+      "swift" => "BPTRTRIS"
+  ],
+  [
+      "name" => "Birleşik Fon Bankası A.Ş.",
+      "address" => "Büyükdere Cad. No 143 Kat 1-2 Esentepe Şişli İstanbul",
+      "telephone" => "212-3401000",
+      "fax" => "212-3473217",
+      "web" => "http://www.fonbank.com.tr",
+      "telex" => "27920 dev TR",
+      "eft" => "0029",
+      "swift" => "BAYDTRIS"
+  ],
+  [
+      "name" => "Burgan Bank A.Ş.",
+      "address" => "Esentepe Mah. Büyükdere Cad. No 209, Tekfen Tower, Şişli İstanbul",
+      "telephone" => "212-3713737",
+      "fax" => "212-3714242",
+      "web" => "http://www.burgan.com.tr",
+      "telex" => null,
+      "eft" => "0125",
+      "swift" => "TEKFTRIS"
+  ],
+  [
+      "name" => "Citibank A.Ş.",
+      "address" => "Saray Mahallesi Ömer Faik Atakan Caddesi, No 3 Yılmaz Plaza Ümraniye İstanbul",
+      "telephone" => "216-5245000",
+      "fax" => "216-5245050",
+      "web" => "http://www.citibank.com.tr",
+      "telex" => "26277 citi TR",
+      "eft" => "0092",
+      "swift" => "CITITRIX"
+  ],
+  [
+      "name" => "Denizbank A.Ş.",
+      "address" => "Büyükdere Cad. No 141 Esentepe Şişli İstanbul",
+      "telephone" => "212-3482000",
+      "fax" => "212-3363030",
+      "web" => "http://www.denizbank.com",
+      "telex" => null,
+      "eft" => "0134",
+      "swift" => "DENITRIS"
+  ],
+  [
+      "name" => "Deutsche Bank A.Ş.",
+      "address" => "Esentepe Mah. Büyükdere Cad. Tekfen Tower, No 209 K:17-18 Şişli İstanbul",
+      "telephone" => "212-3170100",
+      "fax" => "212-3170105",
+      "web" => "http://www.db.com.tr",
+      "telex" => "39343 tmch tr.",
+      "eft" => "0115",
+      "swift" => "BKTRTRIS"
+  ],
+  [
+      "name" => "Diler Yatırım Bankası A.Ş.",
+      "address" => "Tersane Cad. No 96 Diler Han Kat 8 Karaköy İstanbul",
+      "telephone" => "212-2536630",
+      "fax" => "212-2539454",
+      "web" => "http://www.dilerbank.com.tr",
+      "telex" => null,
+      "eft" => "0138",
+      "swift" => "DYAKTRIS"
+  ],
+  [
+      "name" => "Fibabanka A.Ş.",
+      "address" => "Emirhan Caddesi Barbaros Plaza  Merkezi No 113 Dikilitaş  Beşiktaş İstanbul",
+      "telephone" => "212-3818200",
+      "fax" => "212-2583778",
+      "web" => "http://www.fibabanka.com.tr",
+      "telex" => null,
+      "eft" => "0103",
+      "swift" => "FBHLTRIS"
+  ],
+  [
+      "name" => "Finans Bank A.Ş.",
+      "address" => "Esentepe Mah. Büyükdere Cad. Kristal Kule Binası No 215 Şişli İstanbul",
+      "telephone" => "212-3185000",
+      "fax" => "212-3185850",
+      "web" => "http://www.finansbank.com.tr",
+      "telex" => "398242 fnb tr",
+      "eft" => "0111",
+      "swift" => "FNNBTRIS"
+  ],
+  [
+      "name" => "GSD Yatırım Bankası A.Ş.",
+      "address" => "Aydınevler Mah. Kaptan Rıfat Sokak. No 3 GSD Binası No 14 Küçükyalı  Maltepe İstanbul",
+      "telephone" => "216-5879000",
+      "fax" => "216-4899774",
+      "web" => "http://www.gsdbank.com.tr",
+      "telex" => null,
+      "eft" => "0139",
+      "swift" => "GSDBTRIS"
+  ],
+  [
+      "name" => "Habib Bank Limited",
+    "address" => "Abide-i Hürriyet Cad. Geçit Sok. No 6/A Şişli İstanbul",
+    "telephone" => "212-24602202460223",
+    "fax" => "212-2340807",
+    "web" => "http://www.habibbank.com.tr",
+    "telex" => "27849 hbl TR",
+    "eft" => "0097",
+    "swift" => "HABBTRIS"
+  ],
+  [
+      "name" => "HSBC Bank A.Ş.",
+      "address" => "Esentepe Mah. Büyükdere Cad. No 128 Şişli İstanbul",
+      "telephone" => "212-3764000",
+      "fax" => "212-3362939",
+      "web" => "http://www.hsbc.com.tr",
+      "telex" => "38385 mdn TR",
+      "eft" => "0123",
+      "swift" => "HSBCTRIX"
+  ],
+  [
+      "name" => "ING Bank A.Ş.",
+      "address" => "Reşitpaşa Mahallesi Eski Büyükdere Cad. No 8 Sarıyer İstanbul",
+      "telephone" => "212-3351000",
+      "fax" => "212-2866100",
+      "web" => "http://www.ingbank.com.tr",
+      "telex" => null,
+      "eft" => "0099",
+      "swift" => "INGBTRIS"
+  ],
+  [
+      "name" => "Intesa Sanpaolo S.p.A.",
+    "address" => "Meltem Sokak, No: 10  Kuleleri, Kule 2,Kat: 21 Levent Beşiktaş İstanbul",
+    "telephone" => "212-3850600",
+    "fax" => "212-3850649",
+    "web" => "http://www.intesasanpaolo.com.tr",
+    "telex" => null,
+    "eft" => "0148",
+    "swift" => "BCITTRIS"
+  ],
+  [
+      "name" => "İller Bankası A.Ş.",
+    "address" => "Ziraat Mah. 657 Sok. No 14 Dışkapı Ankara",
+    "telephone" => "312-50870003033000",
+    "fax" => "312-50873993107459",
+    "web" => "http://www.ilbank.gov.tr",
+    "telex" => "42723 gnmd tr",
+    "eft" => "0004",
+    "swift" => ""
+  ],
+  [
+      "name" => "İstanbul Takas ve Saklama Bankası A.Ş.",
+    "address" => "Şişli Merkez Mahallesi, Merkez Caddesi, No 6 Şişli İstanbul",
+    "telephone" => "212-3152525",
+    "fax" => "212-3152526",
+    "web" => "http://www.takasbank.com.tr",
+    "telex" => null,
+    "eft" => "0132",
+    "swift" => "TVSBTRIS"
+  ],
+  [
+      "name" => "JPMorgan Chase Bank N.A.",
+      "address" => "Büyükdere Cad. No 185 Kanyon Ofis Binası Kat 8 Levent İstanbul",
+      "telephone" => "212-3198500",
+      "fax" => "212-3198664",
+      "web" => "http://www.jpmorgan.com/pages/international/turkey",
+      "telex" => "26625 cmb TR",
+      "eft" => "0098",
+      "swift" => "CHASTRIS"
+  ],
+  [
+      "name" => "Merrill Lynch Yatırım Bank A.Ş.",
+      "address" => "Büyükdere Cad. No 185 Kanyon Ofisi Bloğu 11. Kat Levent İstanbul",
+      "telephone" => "212-3199500",
+      "fax" => "212-3199511",
+      "web" => "http://www.mlyb.com.tr",
+      "telex" => "25849 tayb TR",
+      "eft" => "0129",
+      "swift" => "MEYYTRISXXX"
+  ],
+  [
+      "name" => "Nurol Yatırım Bankası A.Ş.",
+      "address" => "Maslak Mah. Büyükdere Cad. Nurol Plaza No 257 B Blok, Kat 15 Maslak  Şişli İstanbul",
+      "telephone" => "212-2868100",
+      "fax" => "212-2868101",
+      "web" => "http://www.nurolbank.com.tr",
+      "telex" => null,
+      "eft" => "0141",
+      "swift" => "NUROTRIS"
+  ],
+  [
+      "name" => "Odea Bank A.Ş.",
+      "address" => "Levent 199, Büyükdere Cad. No 199, Kat 33-39 Şişli İstanbul",
+      "telephone" => "212-3048444",
+      "fax" => "212-3048445",
+      "web" => "http://www.odeabank.com.tr",
+      "telex" => null,
+      "eft" => "0146",
+      "swift" => "ODEATRIS"
+  ],
+  [
+      "name" => "Pasha Yatırım Bankası A.Ş.",
+      "address" => "Maslak Mah. A.O.S. 55. Sok. No 2 42 Maslak Ofis 3 Kat 7 Daire 205 Sarıyer İstanbul",
+      "telephone" => "212-7058900",
+      "fax" => "212-3450712",
+      "web" => "http://www.pashabank.com.tr",
+      "telex" => "26475 yatb TR",
+      "eft" => "0116",
+      "swift" => "TAIBTRIS"
+  ],
+  [
+      "name" => "Rabobank A.Ş.",
+      "address" => "Esentepe Mah. Büyükdere Cad. Bahar Sok. River Plaza No: 13 K: 7 Ofis No:15-16 Şişli İstanbul",
+      "telephone" => "212-7084600",
+      "fax" => "212-7084699",
+      "web" => "http://www.rabobank.com.tr",
+      "telex" => null,
+      "eft" => "0137",
+      "swift" => "RABOTRIS"
+  ],
+  [
+      "name" => "Société Générale (SA)",
+      "address" => "Nispetiye Cad. Akmerkez E-3 Blok Kat 10 Etiler İstanbul",
+      "telephone" => "212-3193400",
+      "fax" => "212-28218482821844",
+      "web" => "http://www.sgcib.com.tr",
+      "telex" => "SOGETRIS",
+      "eft" => "0122",
+      "swift" => "SOGETRIS"
+  ],
+  [
+      "name" => "Standard Chartered Yatırım Bankası Türk A.Ş.",
+      "address" => "Büyükdere Cad. Yapı Kredi Plaza, C Blok, Kat 15 Levent İstanbul",
+      "telephone" => "212-3393700",
+      "fax" => "212-2826301",
+      "web" => "http://www.standardchartered.com.tr",
+      "telex" => null,
+      "eft" => "0121",
+      "swift" => "BSUITRIS"
+  ],
+  [
+      "name" => "Şekerbank T.A.Ş.",
+      "address" => "Büyükdere Caddesi No 171 Metrocity A- Blok Esentepe  Mecidiyeköy İstanbul",
+      "telephone" => "212-3197000",
+      "fax" => "212-3197429",
+      "web" => "http://www.sekerbank.com.tr",
+      "telex" => "46990 skgm- TR eker TR",
+      "eft" => "0059",
+      "swift" => "SEKETR2A"
+  ],
+  [
+      "name" => "Tekstil Bankası A.Ş.",
+      "address" => "Maslak Mah. Dereboyu/2 Cad. No 13 Sarıyer İstanbul",
+      "telephone" => "212-3355335",
+      "fax" => "212-3281328",
+      "web" => "http://www.tekstilbank.com.tr",
+      "telex" => "39381 tbak TR",
+      "eft" => "0109",
+      "swift" => "TEKBTRIS"
+  ],
+  [
+      "name" => "The Royal Bank of Scotland Plc.",
+      "address" => "Tamburi Ali Efendi Sok. No 13 Etiler Beşiktaş İstanbul",
+      "telephone" => "212-3594040",
+      "fax" => "212-3595050",
+      "web" => "http://www.rbsbank.com.tr",
+      "telex" => "24677 hbu TR",
+      "eft" => "0088",
+      "swift" => "ABNATRIS"
+  ],
+  [
+      "name" => "Turkish Bank A.Ş.",
+      "address" => "Vali Konağı Cad. No 1 Nişantaşı Şişli İstanbul",
+      "telephone" => "212-3736373",
+      "fax" => "212-225035355",
+      "web" => "http://www.turkishbank.com.tr",
+      "telex" => "27359 tblı TR",
+      "eft" => "0096",
+      "swift" => "TUBATRIS"
+  ],
+  [
+      "name" => "Turkland Bank A.Ş.",
+      "address" => "19 Mayıs Mah. 19 Mayıs Cad. Şişli Plaza A Blok No 7 Şişli İstanbul",
+      "telephone" => "212-3683434",
+      "fax" => "212-3683535",
+      "web" => "http://www.tbank.com.tr",
+      "telex" => "25461-25462",
+      "eft" => "0108",
+      "swift" => "TBNKTRIS"
+  ],
+  [
+      "name" => "Türk Ekonomi Bankası A.Ş.",
+      "address" => "TEB Kampüs C ve D Blok Saray Mah. Sokullu Cad. No. 7A-7B 34768 Ümraniye İstanbul",
+      "telephone" => "216-6353535",
+      "fax" => "216-6363636",
+      "web" => "http://www.teb.com.tr",
+      "telex" => "25358 tebu TR",
+      "eft" => "0032",
+      "swift" => "TEBUTRIS"
+  ],
+  [
+      "name" => "Türk Eximbank",
+      "address" => "Saray Mahallesi Ahmet Tevfik İleri Cad. No 19 Ümraniye İstanbul",
+      "telephone" => "216-6665500",
+      "fax" => "216-6665599",
+      "web" => "http://www.eximbank.gov.tr",
+      "telex" => "46751 exmb TR",
+      "eft" => "0016",
+      "swift" => "TIKBTR2A"
+  ],
+  [
+      "name" => "Türkiye Cumhuriyeti Ziraat Bankası A.Ş.",
+      "address" => "Anafartalar Mah. Atatürk Bulvarı No 8 Altındağ Ankara",
+      "telephone" => "312-5842000",
+      "fax" => "312-5842551",
+      "web" => "http://www.ziraatbank.com.tr",
+      "telex" => "44004 zbhm TR",
+      "eft" => "0010",
+      "swift" => "TCZBTR2A"
+  ],
+  [
+      "name" => "Türkiye Garanti Bankası A.Ş.",
+      "address" => "Nispetiye Mah. Aytar Cad. No 2 Levent Beşiktaş İstanbul",
+      "telephone" => "212-3181818",
+      "fax" => "212-3181888",
+      "web" => "http://www.garanti.com.tr",
+      "telex" => "27635 gatı TR",
+      "eft" => "0062",
+      "swift" => "TGBATRIS"
+  ],
+  [
+      "name" => "Türkiye Halk Bankası A.Ş.",
+      "address" => "Barbaros Mahallesi, Şebboy Sokak No:4 Ataşehir İstanbul",
+      "telephone" => "216-5037070",
+      "fax" => "212-3409399",
+      "web" => "http://www.halkbank.com.tr",
+      "telex" => "TRHBTR2A",
+      "eft" => "0012",
+      "swift" => "TRHBTR2A"
+  ],
+  [
+      "name" => "Türkiye İş Bankası A.Ş.",
+      "address" => "Kuleleri Levent Beşiktaş İstanbul",
+      "telephone" => "212-3160000",
+      "fax" => "212-3160900-05",
+      "web" => "http://www.isbank.com.tr",
+      "telex" => "42082 tab TR",
+      "eft" => "0064",
+      "swift" => "ISBKTRIS"
+  ],
+  [
+      "name" => "Türkiye Kalkınma Bankası A.Ş.",
+      "address" => "Necatibey Caddesi, No 98 Yenişehir Ankara",
+      "telephone" => "312-2318400312-2313125",
+      "fax" => "312-2313125",
+      "web" => "http://www.kalkinma.com.tr",
+      "telex" => "43206 turb-TR",
+      "eft" => "0017",
+      "swift" => "TKBNTR2A"
+  ],
+  [
+      "name" => "Türkiye Sınai Kalkınma Bankası A.Ş.",
+      "address" => "Meclisi Mebusan Cad. No 81 Fındıklı İstanbul",
+      "telephone" => "212-3345050",
+      "fax" => "212-3345234",
+      "web" => "http://www.tskb.com.tr",
+      "telex" => "24344 tskb TR",
+      "eft" => "0014",
+      "swift" => "TSKBTRIS"
+  ],
+  [
+      "name" => "Türkiye Vakıflar Bankası T.A.O.",
+      "address" => "Sanayi Mahallesi Eski Büyükdere Caddesi Güler Sokak No: 51 Kağıthane İstanbul",
+      "telephone" => "2123981515-2123981000",
+      "fax" => "2123981155",
+      "web" => "http://www.vakifbank.com.tr",
+      "telex" => "46023 vbho-tr",
+      "eft" => "0015",
+      "swift" => "TVBATR2A"
+  ],
+  [
+      "name" => "Yapı ve Kredi Bankası A.Ş.",
+      "address" => "Yapı Kredi Plaza D Blok Levent İstanbul",
+      "telephone" => "212-3397000",
+      "fax" => "212-3396000",
+      "web" => "http://www.yapikredi.com.tr",
+      "telex" => "931002 ykge TR",
+      "eft" => "0067",
+      "swift" => "YAPITRISFEX"
+  ]
+];
